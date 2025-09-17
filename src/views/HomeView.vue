b13c19ef0ad8367c25b5dfa1053374cfe588fa71
liuziting <57311725+liu-ziting@users.noreply.github.com>
2025-09-17 20:28:17 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -51,7 +51,7 @@
                                             <svg class="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 20 20">
                                                 <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path>
                                             </svg>
-                                            <span>📷 使用豆包</span>
+                                            <span>豆包</span>
                                         </a>
                                         <span class="text-gray-500 text-sm sm:text-base font-medium">或</span>
                                         <a
@@ -63,7 +63,7 @@
                                             <svg class="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 20 20">
                                                 <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path>
                                             </svg>
-                                            <span>📷 使用 LMArena</span>
+                                            <span>LMArena</span>
                                         </a>
                                     </div>
                                 </div>
